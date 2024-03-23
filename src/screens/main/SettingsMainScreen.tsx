5c5969298571250002cb18c813c9e7a0462809fe
hwanheejung <hwanheejung117@gmail.com>
2024-03-23 23:18:06 +0900

    #8 feat: 설정페이지

--- src/screens/main/SettingsMainScreen.tsx
+++ src/screens/main/SettingsMainScreen.tsx
@@ -3,10 +3,21 @@ import Icons from "@/components/@common/Icons";
 import PageHeader from "@/components/@common/PageHeader";
 import Typography from "@/components/@common/Typography";
 import { spacing } from "@/constants/spacing";
+import { THEME } from "@/constants/theme";
+import useResponsiveSize from "@/utils/useResponsiveSize";
 import React from "react";
 import { TouchableOpacity, View } from "react-native";
+import styled from "styled-components/native";
 
-const Row = ({ onPress, text }: { onPress?: () => void; text: string }) => {
+const Row = ({
+  onPress,
+  text,
+  color,
+}: {
+  onPress?: () => void;
+  text: string;
+  color?: string;
+}) => {
   return (
     <TouchableOpacity onPress={onPress}>
       <FlexBox
@@ -14,10 +25,12 @@ const Row = ({ onPress, text }: { onPress?: () => void; text: string }) => {
         justifyContent="space-between"
         styles={{
           paddingHorizontal: spacing.gutter,
-          paddingVertical: spacing.padding,
+          paddingVertical: useResponsiveSize(14),
         }}
       >
-        <Typography size="md">{text}</Typography>
+        <Typography size="md" color={color ? color : "black"}>
+          {text}
+        </Typography>
         <Icons type="feather" name="chevron-right" size={30} />
       </FlexBox>
     </TouchableOpacity>
@@ -30,7 +43,7 @@ const Version = ({ version }: { version: string }) => (
     justifyContent="space-between"
     styles={{
       paddingHorizontal: spacing.gutter,
-      paddingVertical: spacing.offset,
+      paddingVertical: useResponsiveSize(14),
     }}
   >
     <Typography size="md">버전 정보</Typography>
@@ -38,14 +51,55 @@ const Version = ({ version }: { version: string }) => (
   </FlexBox>
 );
 
+const SectionContainer = styled.View`
+  background-color: ${THEME.palette.gray};
+  border-radius: 20px;
+  padding: ${spacing.offset}px 0;
+`;
+
+const Section = ({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title: string;
+}) => (
+  <View
+    style={{
+      marginVertical: spacing.padding,
+      marginHorizontal: spacing.offset,
+      gap: spacing.padding,
+    }}
+  >
+    <Typography
+      size="sm"
+      color={THEME.palette.subText}
+      styles={{
+        paddingLeft: spacing.padding,
+      }}
+    >
+      {title}
+    </Typography>
+    <SectionContainer>{children}</SectionContainer>
+  </View>
+);
+
 const SettingsMainScreen = () => {
   return (
     <View>
       <PageHeader title="설정" />
-      <Version version="1.0.0" />
-      <Row text="개인정보 처리방침" onPress={() => {}} />
-      <Row text="이용 약관" onPress={() => {}} />
-      <Row text="오픈소스 라이브러리" onPress={() => {}} />
+
+      <Section title="계정 설정">
+        <Row text="닉네임 변경" onPress={() => {}} />
+        <Row text="비밀번호 변경" onPress={() => {}} />
+      </Section>
+      <Section title="앱 정보">
+        <Version version="1.0.0" />
+        <Row text="개인정보 처리방침" onPress={() => {}} />
+        <Row text="이용 약관" onPress={() => {}} />
+        <Row text="오픈소스 라이브러리" onPress={() => {}} />
+      </Section>
+      <Row text="로그아웃" onPress={() => {}} color={THEME.palette.red} />
     </View>
   );
 };
